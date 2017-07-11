17b29a55ccc75e3e71de1ab79ba5e2b8bb019f6c
George Vuong <georgevuong18@gmail.com>
2017-07-11 08:03:03 -0700

    refactor logger by writing applyMiddlewares function which passes a store and each middleware

--- W7D1_HW_Implement_Middleware/Intro_to_Middleware/frontend/todo_redux.jsx
+++ W7D1_HW_Implement_Middleware/Intro_to_Middleware/frontend/todo_redux.jsx
@@ -10,23 +10,61 @@ document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore(preloadedState);
 
   // Phase 1: Logging
-  store.dispatch = addLoggingToDispatch(store);
+  // store.dispatch = addLoggingToDispatch(store);
+
+  // Phase 2: Refactoring (without using Redux applyMiddleware)
+  //We need to pass in the store and each middleware that we want to apply. In this case, addLoggingToDispatch is the only middleware.
+  store = applyMiddlewares(store, addLoggingToDispatch);
 
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
 });
 
 // Phase 1: Logging
-const addLoggingToDispatch = store => {
-  const x = store.dispatch;
-  return (action) => {
-    console.log(store.getState());
-    console.log(action);
-    x(action);
-    console.log(store.getState());
-  };
+// const addLoggingToDispatch = store => {
+//   const x = store.dispatch;
+//   return (action) => {
+//     console.log(store.getState());
+//     console.log(action);
+//     x(action);
+//     console.log(store.getState());
+//   };
+// };
+
+// Phase 2: Refactoring (without using Redux applyMiddleware)
+// function addLoggingToDispatch(store) {
+//   return function (nextMiddleware) {
+//     return function (action) {
+//       console.log('action received: ', store.getState());
+//       console.log('state pre-dispatch: ', action);
+//
+//       let res = nextMiddleware(action);
+//       console.log('state post-dispatch: ', store.getState());
+//
+//       return res;
+//     };
+//   };
+// }
+
+// OR ES6 way:
+const addLoggingToDispatch = store => nextMiddleware => action => {
+  console.log('action received: ', action);
+  console.log('state pre-dispatch: ', store.getState());
+
+  // let res = nextMiddleware(action);
+  console.log('state post-dispatch: ', store.getState());
+
+  // return res;
 };
 
-let store = configureStore();
-window.store = store;
+const applyMiddlewares = (store, ...middlewares) => {
+  let dispatch = store.dispatch;
+  middlewares.forEach((middleware) => {
+    dispatch = middleware(store, dispatch);
+  });
+  return Object.assign({}, store, {dispatch});
+};
+
+// let store = configureStore();
+// window.store = store;
 // store.dispatch = addLoggingToDispatch(store);
